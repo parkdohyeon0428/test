a47cb05dc7f1ecb799888e25a9074735e1915012
dohyeon <pkppfh@naver.com>
2025-03-25 23:03:27 +0900

    0325

--- 250319_uart_memory/250319_uart_memory.srcs/sources_1/new/ram_ip.v
+++ 250319_uart_memory/250319_uart_memory.srcs/sources_1/new/ram_ip.v
@@ -1,14 +1,18 @@
 `timescale 1ns / 1ps
 
-module ram_ip #(parameter ADDR_WIDTH = 4, DATA_WIDTH= 8)(
-    input                   clk,
-    input  [ADDR_WIDTH-1:0] waddr,
-    input  [DATA_WIDTH-1:0] wdata,
-    input                   wr,
-    output [DATA_WIDTH-1:0] rdata
-    );
+module ram_ip #(
+    parameter ADDR_WIDTH = 4,  // 주소 크기 (4비트 = 16개의 저장공간)
+    parameter DATA_WIDTH = 8   // 데이터 크기 (8비트 = 1바이트)
+)(
+    input                   clk,       // 클럭
+    input  [ADDR_WIDTH-1:0] waddr,     // 쓰기 주소 (4비트)
+    input  [DATA_WIDTH-1:0] wdata,     // 쓰기 데이터 (8비트)
+    input                   wr,        // 쓰기 활성화 (1이면 쓰기, 0이면 읽기)
+    output [DATA_WIDTH-1:0] rdata      // 읽기 데이터 (8비트)
+);
     //reg [DATA_WIDTH-1:0] rdata_reg;
-    reg [DATA_WIDTH-1:0] ram[0:2**ADDR_WIDTH-1];    // 2**4 = 2의 4승
+    reg [DATA_WIDTH-1:0] ram[0:2**ADDR_WIDTH-1];    // 2**4 = 2의 4승  
+          //해당 비트          // 저장공간         //16개의 8비트 저장공간
     // write
     always @(posedge clk) begin
         if (wr) begin
