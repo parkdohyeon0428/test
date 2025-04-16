99215d6e6cdfab2f41eb1cfc7cc2f99ab1778175
dohyeon <pkppfh@naver.com>
2025-04-16 16:54:13 +0900

    0416

--- 250416_hee_rv32i/250416_hee_rv32i.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/xsim_webtalk.tcl
+++ 250416_hee_rv32i/250416_hee_rv32i.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir D:/test/250411_RV32i_J_Type/250411_RV32i_J_Type.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/
+webtalk_init -webtalk_dir D:/test/250416_hee_rv32i/250416_hee_rv32i.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Apr 16 15:14:45 2025" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Apr 16 16:54:04 2025" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -13,8 +13,8 @@ webtalk_add_data -client project -key target_device -value "not_applicable" -con
 webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "3216aef5-5b25-4004-9ec5-b2f89c651963" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_id -value "aae9db3ccea64caebd66f130053e0a5c" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "73" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_id -value "6b475618c8e64190b73ab911411fc7a8" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "12th Gen Intel(R) Core(TM) i7-1260P" -context "user_environment"
@@ -25,9 +25,9 @@ webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "11024465 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "18550445 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.36_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "16624_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4051930553 -regid "" -xml D:/test/250411_RV32i_J_Type/250411_RV32i_J_Type.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/test/250411_RV32i_J_Type/250411_RV32i_J_Type.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/test/250411_RV32i_J_Type/250411_RV32i_J_Type.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.53_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "16644_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2078992680 -regid "" -xml D:/test/250416_hee_rv32i/250416_hee_rv32i.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/test/250416_hee_rv32i/250416_hee_rv32i.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/test/250416_hee_rv32i/250416_hee_rv32i.sim/sim_1/behav/xsim/xsim.dir/tb_RV32I_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
