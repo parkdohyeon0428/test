0ef5c8861b681bb8650938ffeb42bc51b169f9bd
dohyeon <pkppfh@naver.com>
2025-05-18 00:04:56 +0900

    0517

---
+++ 250516_SPI_Slave_Master/250516_SPI_Slave_Master.srcs/sim_1/new/tb_SPI.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+module tb_SPI();
+
+    reg clk;
+    reg reset;
+    reg btn;
+    reg [15:0] sw;
+    wire [3:0] fndcom;
+    wire [7:0] fndfont;    
+    
+    SPI dut(
+    .clk(clk),
+    .reset(reset),
+    .btn(btn),
+    .sw(sw),
+    .fndcom(fndcom),
+    .fndfont(fndfont)
+    );
+    
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        reset = 1;
+        btn = 0;
+        sw = 0;
+        #10;
+        reset = 0;
+        #10;
+        btn = 1;
+        sw = 16'hff11;
+        #100;
+        btn = 0;
+    end
+
+endmodule
